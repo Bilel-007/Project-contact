44d8bd2d13111649b11adf2fd9fecb7d00f0ac05
Bilel-707 <Bilel.bouneb90@gmail.com>
2022-09-15 15:39:05 +0100

    dockerisation en cours

--- back/config/connectDB.js
+++ back/config/connectDB.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 // require('dotenv'.config({path:'./.env'}))
 // require('dotenv').config()
 // MONGO_URI=process.env.MONGO_URI
 
-const connectDB=()=>{
-    mongoose.connect("mongodb://localhost:27017",(err)=>{
-        if(err) throw err
-        else console.log("database is connected")
-    })
-}
-module.exports=connectDB
+const connectDB = () => {
+  mongoose.connect("mongodb://mongo:27017", (err) => {
+    if (err) throw err;
+    else console.log("database is connected");
+  });
+};
+module.exports = connectDB;
